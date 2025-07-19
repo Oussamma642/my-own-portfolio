e5944cb1ef4ff873a782b7be4fb9947a1e2c2d43
AIT-MOHAMED Oussama <sadosami297@gmail.com>
2025-07-19 14:52:35 +0100

    Link of educations

--- src/App.jsx
+++ src/App.jsx
@@ -106,7 +106,8 @@ const Portfolio = () => {
       tech: ["Laravel", "React", "MySQL", "Tailwind CSS"],
       image: "./projects/notes.jpeg",
       demo: "https://github.com/Oussamma642/PFE-gestion-d-un-centre-de-formation",
-      projectDemoLinkdine:"https://www.linkedin.com/posts/azzedine-rih-a284b02a0_daezveloppementweb-laravel-reactjs-ugcPost-7342252894135496704-iukz?utm_source=share&utm_medium=member_desktop&rcm=ACoAAFRr3s0BqxrX5Ub5RpNmesdbcHMBVgfzN10"
+      projectDemoLinkdine:
+        "https://www.linkedin.com/posts/azzedine-rih-a284b02a0_daezveloppementweb-laravel-reactjs-ugcPost-7342252894135496704-iukz?utm_source=share&utm_medium=member_desktop&rcm=ACoAAFRr3s0BqxrX5Ub5RpNmesdbcHMBVgfzN10",
     },
     {
       id: 2,
@@ -116,7 +117,8 @@ const Portfolio = () => {
       tech: ["Laravel", "React", "MySQL", "Tailwind CSS"],
       image: "./projects/formations.jpg",
       demo: "https://github.com/Oussamma642/projet-de-synthese-gestion-formations-formateurs",
-      projectDemoLinkdine:"https://www.linkedin.com/posts/oussama-ait-mohamed-239402336_daezveloppementweb-laravel-reactjs-activity-7336102076525010946-5b5k?utm_source=share&utm_medium=member_desktop&rcm=ACoAAFRr3s0BqxrX5Ub5RpNmesdbcHMBVgfzN10"
+      projectDemoLinkdine:
+        "https://www.linkedin.com/posts/oussama-ait-mohamed-239402336_daezveloppementweb-laravel-reactjs-activity-7336102076525010946-5b5k?utm_source=share&utm_medium=member_desktop&rcm=ACoAAFRr3s0BqxrX5Ub5RpNmesdbcHMBVgfzN10",
     },
     {
       id: 3,
@@ -126,7 +128,8 @@ const Portfolio = () => {
       tech: ["Laravel", "Blade templating engine", "MySQL"],
       image: "./projects/contacts.jpg",
       demo: "https://github.com/Oussamma642/contact-management-system",
-      projectDemoLinkdine:"https://github.com/Oussamma642/contact-management-system"
+      projectDemoLinkdine:
+        "https://github.com/Oussamma642/contact-management-system",
     },
   ];
 
@@ -751,8 +754,12 @@ const Portfolio = () => {
                                   <path d="M12 0C5.373 0 0 5.373 0 12c0 5.302 3.438 9.8 8.207 11.387.6.11.793-.26.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23A11.509 11.509 0 0112 5.803c1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576C20.566 21.797 24 17.3 24 12c0-6.627-5.373-12-12-12z" />
                                 </svg>
                               </div>
-                               <div className="w-6 h-6 text-white hover:text-indigo-400 transition-colors duration-200">
-                                <a href={project.projectDemoLinkdine} target="_blank" rel="noopener noreferrer">
+                              <div className="w-6 h-6 text-white hover:text-indigo-400 transition-colors duration-200">
+                                <a
+                                  href={project.projectDemoLinkdine}
+                                  target="_blank"
+                                  rel="noopener noreferrer"
+                                >
                                   <ExternalLink className="w-6 h-6" />
                                 </a>
                               </div>
@@ -812,19 +819,17 @@ const Portfolio = () => {
                             Source Code
                           </a>
                         )}
-                        {
-                          project.projectDemoLinkdine && (
-                            <a
-                              href={project.projectDemoLinkdine}
-                              target="_blank"
-                              rel="noopener noreferrer"
-                              className="bg-indigo-600 hover:bg-indigo-700 text-white px-6 py-2 rounded-md transition-colors duration-200 text-sm font-medium"
-                            >
-                              <ExternalLink className="inline-block mr-2 w-4 h-4" />
-                              Project Demo
-                            </a>
-                          )
-                        }
+                        {project.projectDemoLinkdine && (
+                          <a
+                            href={project.projectDemoLinkdine}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="bg-indigo-600 hover:bg-indigo-700 text-white px-6 py-2 rounded-md transition-colors duration-200 text-sm font-medium"
+                          >
+                            <ExternalLink className="inline-block mr-2 w-4 h-4" />
+                            Project Demo
+                          </a>
+                        )}
                       </div>
                     </div>
                   </div>
@@ -833,7 +838,6 @@ const Portfolio = () => {
             </div>
           </div>
         </section>
-
 
         {/* Experience & Education Section */}
         <section id="experience" className="py-20 fade-in">
@@ -867,15 +871,21 @@ const Portfolio = () => {
                       }`}
                     >
                       <div className="bg-slate-900/95 p-6 rounded-lg shadow-md md:ml-0">
-                        <div className="text-indigo-200 font-semibold text-sm mb-1">
+                        <div className="text-indigo-200 font-semibold text-xl text-sm mb-1">
                           {item.year}
                         </div>
-                        <div className="text-indigo-200 font-medium mb-2">
+                        <div className="text-indigo-200 text-xl font-medium mb-2">
                           {item.company}
                         </div>
-                        <h3 className="text-xl font-semibold text-indigo-400 mb-1">
-                          <a target="_blank" href={item.link}>
+                        <h3 className="font-semibold text-indigo-400 mb-1">
+                          <a
+                            target="_blank"
+                            href={item.link}
+                            className="underline hover:text-indigo-300 transition-colors cursor-pointer inline-flex items-center gap-2"
+                            title="Open link"
+                          >
                             {item.title}
+                            <ExternalLink className="w-4 h-4" />
                           </a>
                         </h3>
 
